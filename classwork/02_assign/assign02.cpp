7a3cd2976042699d5936e7d502f8039cfa599d05
8385211 <8385211@RRC2220-03>
2019-01-29 19:23:56 -0600

    Assignment 2

--- classwork/02_assign/assign02.cpp
+++ classwork/02_assign/assign02.cpp
@@ -1,1 +1,7 @@
 //Write the function code that returns the product of hours and hourly_rate.
+#include"assign02.h"
+
+double gross_pay(double hour, double hourly_rate)
+{
+	return hour * hourly_rate;
+}
