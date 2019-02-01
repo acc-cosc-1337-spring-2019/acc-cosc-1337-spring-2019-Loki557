243318fe15f9be14bcca8276733f35a1fe7db63d
8385211 <8385211@RRC2220-03>
2019-01-31 19:17:32 -0600

    Conditional examples

--- examples/01_module/06_if_else/if_else.cpp
+++ examples/01_module/06_if_else/if_else.cpp
@@ -1,3 +1,4 @@
+#include "if_else.h"
 //write include statement for if_else header file
 
 //write code for function named get_generation that accepts an int year and returns
@@ -8,4 +9,32 @@
 //return 'Baby boomer' when year greater than equal to 1946 to 1964
 //return 'Silent Generation' when year greater than equal 1925 and less than equal 1945
 //return 'Invalid Year' otherwise
+using std::string;
+std::string get_generation(int year)
+{
+	if (year >= 1996 && year <= 2014)
+	{
+		return "Centenial";
+	}
+	else if (year >= 1977 && year <= 1995)
+	{
+		return "Millenial";
+	}
+	else if (year >= 1965 && year <= 1976)
+	{
+		return "Generation X";
+	}
+	else if (year >= 1946 && year <= 1964)
+	{
+		return "Baby Boomer";
+	}
+	else if (year >= 1925 && year <= 1945)
+	{
+		return "Silent Generation";
+	}
+	else
+	{
+		return "Invalid Year";
+	}
 
+}
