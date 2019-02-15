beb2ea71324056755a6e8f0e954af2995548540a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2019-02-15 10:20:19 -0600

    remove commented code

--- examples/03_module/06_value_and_reference_params/sample_value_ref.cpp
+++ examples/03_module/06_value_and_reference_params/sample_value_ref.cpp
@@ -1,8 +1,8 @@
-//#include "sample_value_ref.h"
-//
-//void pass_by_val_and_ref(int num1, int &num2, const int &num3)
-//{
-//	num1 = 20;
-//	num2 = 50; //Modifying this value will potentially change caller variable
-//	//num3 = 100; can't be modified, const variable
-//}
+#include "sample_value_ref.h"
+
+void pass_by_val_and_ref(int num1, int &num2, const int &num3)
+{
+	num1 = 20;
+	num2 = 50; //Modifying this value will potentially change caller variable
+	//num3 = 100; can't be modified, const variable
+}
